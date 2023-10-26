b1a97089f6631a5078dba1edfff5d13443ff402f
Proyect0Yv4n <yvanindustrial@gmail.com>
2023-10-26 10:36:33 -0400

    #3 correccion momentanea de modelo para verificar compilacion de proyecto

--- backend/Modelo/Alumno.cs
+++ backend/Modelo/Alumno.cs
@@ -6,12 +6,22 @@ namespace Modelo
 {
     public class Alumno
     {
-            [Key]
-            [Column("id")]
-       //completar el modelo segun los datos que se necesitan para el modelo de alumno
-
-
-
+        [Key]
+        [Column("id")]
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public string? primerRegistro { get; set; }
+        public string? correoPersonal { get; set; }
+        public string? correoUdabol { get; set; }
+        public string? codigo { get; set; }
+        public string? verificacion { get; set; }
+        public bool isActive { get; set; }
+        public int ex_GIT_6PTS { get; set; }
+        public int ex_ADOO_8PTS { get; set; }
+        public int ex_ScrumTra_41PTS { get; set; }
+        public int ex_SOLID_8PTS { get; set; }
+        public int ex_12FactApp_12PTS { get; set; }
+        public int ex_Docker_5PTS { get; set; }
     }
 }
 
